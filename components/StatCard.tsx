c88484a74f3f9e29ebfaeb6d43bb39194182c8fb
Vaibhav Verma <animusabstergo7@gmail.com>
2024-09-13 18:13:39 +0530

    admin dashboard table

--- components/StatCard.tsx
+++ components/StatCard.tsx
@@ -1,3 +1,5 @@
+import clsx from 'clsx'
+import Image from 'next/image'
 import React from 'react'
 
 
@@ -10,7 +12,24 @@ interface StatCardProps {
 
 const StatCard = ({ count= 0, label, icon, type }: StatCardProps) => {
   return (
-    <div className=''>
+    <div className={clsx('stat-card', {
+      'bg-appointments': type === 'appointments',
+      'bg-pending': type === 'pending',
+      'bg-cancelled': type === 'cancelled',
+    })}>
+      <div className='flex items-center gap-4'>
+        <Image
+        src={icon}
+        height={32}
+        width={32}
+        alt={label}
+        className='size-8 w-fit'>
+        </Image>
+
+        <h2 className='text-32-bold text-white'>{count}</h2>
+      </div>
+
+      <p className='text-14-regular text-white'>{label}</p>
 
     </div>
   )
--- lib/actions/appointment.actions.ts
+++ lib/actions/appointment.actions.ts
@@ -1,8 +1,9 @@
 'use server';
 
-import { ID } from "node-appwrite";
+import { ID, Query } from "node-appwrite";
 import { APPOINTMENT_COLLECTION_ID, DATABASE_ID, databases, PATIENT_COLLECTION_ID } from "../appwrite.config";
 import { parseStringify } from "../utils";
+import { Appointment } from "@/types/appwrite.types";
 
 export const createAppointment = async (appointment: CreateAppointmentParams) => {
     try {
@@ -31,5 +32,44 @@ export const getAppointment = async (appointmentId: string) => {
         console.log(error);
     }
 }
-    
-    
+
+export const getRecentAppointmentList = async () => {
+    try {
+        const appointments = await databases.listDocuments(
+            DATABASE_ID!,
+            APPOINTMENT_COLLECTION_ID!,
+            [Query.orderDesc('$createdAt')]
+        );
+
+        const initialCounts = {
+            scheduledCount: 0,
+            pendingCount: 0,
+            cancelledCount: 0,
+        }
+
+        const counts = (appointments.documents as Appointment[]).reduce((acc, appointment) => {
+            if (appointment.status === 'scheduled') {
+                acc.scheduledCount += 1;
+            } else if (appointment.status === 'pending') {
+                acc.pendingCount += 1;
+            } else if (appointment.status === 'cancelled') {
+                acc.cancelledCount += 1;
+            }
+
+            return acc;
+        }, initialCounts);
+
+        const data = {
+            totalCount: appointments.total,
+            ...counts,
+            documents: appointments.documents
+        }
+
+        return parseStringify(data);
+
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+
